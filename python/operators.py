d73ab3a3e3cbf60a55693920cb8f99dcb502cdd5
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-02-24 13:45:41 +0530

    .

---
+++ python/operators.py
@@ -0,0 +1,75 @@
+#arithmetic ops
+print(7 +3)
+
+print(5-4)
+
+print(5*6)
+
+print(10/2)
+
+print(5//2)   #rounds to integer format
+
+print(5%2)
+
+print(5**2)  #squaring
+print(3**3)
+
+
+#relational operators
+
+print(4>5)
+
+print(4>=4)
+
+
+print(4<5)
+
+
+print(4<=5)
+
+print(4==4)
+
+print(4!=4)
+
+#LOGICAL OPS
+#and
+print(1 and 0)
+
+#or
+print(1 or 0)
+
+#not
+print(not 1)
+
+
+#BITWISE OPS
+print(2 & 3) #bitwise and
+
+print(2 | 3) #bitwise or
+
+print(2 ^ 3) #bitwise xor  --> aagar dono same toh o warna 1
+
+print(~3)   #bitwise not
+
+print(4 >> 2) #bitwise right
+
+print(4<<2 ) #bitwise left
+
+
+
+#ASSIGNMENT OPS
+# = `this is assignment operator`
+a=2
+a+=2  #can use +,-*,% etc.   #a++ , ++a is tarah se notation ni krte h
+print(a)
+
+
+
+#MEMBERSHIP OPS
+#in/not in -------> kisi given chiz m koi aur chiz h ki ni
+print('D' in 'Dehradun')
+
+print(1 in [2,3,4,5,6,7,8]) # 1 nai h toh false
+#not in
+print('S'  not in 'Shimla')   # S hai isliey false
+
