94b181cff41fc854f4b538188cf4dcdf6285ec84
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-02-27 14:48:58 +0530

    Create min of 3.py

---
+++ python/if else/min of 3.py
@@ -0,0 +1,12 @@
+a=int(input('first num'))
+b=int(input('second num'))
+c=int(input('third num'))
+
+
+if a<b and a<c :
+ print('a is smallest',a)
+elif b<a and b<c :
+    print( ' b is smallest',b)
+
+else :
+    print('smallest is',c)
