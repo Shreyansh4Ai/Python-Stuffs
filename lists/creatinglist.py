beef1648f1ee2bcb663d6b58cfe6ff9b60c9a740
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-03-30 17:45:57 +0530

    .py

--- lists/creatinglist.py
+++ lists/creatinglist.py
@@ -3,3 +3,8 @@ l=[1,2,3,4] #homo 1D
 l1=[1,2,3,[4,5]] #hetero contains a list inside list 
 
 l2=[[[1,2],[3,4],[5,6]]] #3D homo
+
+l3=[1,True,5.6,5+6j,'hello'] #heterogenous
+
+#type conversion
+print(list('hello'))
