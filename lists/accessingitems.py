72ea17040ffe61db8abf9a529067ef10b08f5b08
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-03-30 18:09:42 +0530

    .py

---
+++ lists/accessingitems.py
@@ -0,0 +1,21 @@
+l=[1,2,3,4]   #positive indexing
+print(l[0])
+
+#-ve indexing
+print(l[-3])
+
+print(l[-1])
+
+
+l1=[1,2,3,[4,5]]
+
+print(l1[-1])
+print(l1[-1][-2])  # second square braces are for getting inside the list 
+print(l1[3][0])  #positive indexing 
+
+
+a=[[[1,2],[3,4],[5,6],[7,8]]]
+#positive
+print(a[0])
+
+print(a[0][0][1])
---
+++ lists/slicing.py
@@ -0,0 +1,13 @@
+l=[1,2,3,4,5,6]
+
+print(l[0:3])
+
+print(l[-3:])
+
+print(l[0::2]) #alternative items extraction )
+
+print(l[-5:-2:2]) 
+
+print(l[::-1]) #reverse the list 
+
+
