f32aba7ced7570f323e4d12bcff64a5d52fbd945
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-04-01 00:59:09 +0530

    Create creatingTupple.py

---
+++ tupples+set+dictionary/creatingTupple.py
@@ -0,0 +1,15 @@
+  #empty
+t1=()
+print (t1)
+
+#create a tupple with single item
+t2 =(2)
+print(type(t2))   #not a tupple
+
+#homo
+t3 =(1,2,3,4,)
+print(t3)
+
+#hetero
+t4=(1,2.5,True,[1,2,3])
+print(t4)
