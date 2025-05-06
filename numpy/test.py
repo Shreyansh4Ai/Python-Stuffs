b6eb4a097cf1632b0ac5ecbfcb5e17b391c860fb
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2025-05-06 21:59:09 +0530

    Create test.py

---
+++ numpy/test.py
@@ -0,0 +1,9 @@
+#use command pip install numpy 
+#it is used for installing and updating the numpy in the vs editor terminal 
+
+
+#importing numpy
+import numpy as nd # type: ignore
+arr = nd.array([1,2,3,4,5,6,7,8,9,10])
+
+print(arr)
